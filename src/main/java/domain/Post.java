8897888fded1d194811eeadf5c50ac05011f747e
Vladislav Semenov <arctic.ratatat@gmail.com>
2021-12-14 21:33:09 +0300

    initial commit

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String title;
+    private String imageUrl;
+    private String date;
+    private String time;
+    private String body;
+    private String likeButton;
+    private String commentsButton;
+    private String shareButton;
+    private String views;
+}
